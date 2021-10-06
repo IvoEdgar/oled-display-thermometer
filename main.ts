a90c2dba61e7a212d090493a6b7b3542930d51f6
Ivo Edgar <ivo.edgy@gmail.com>
2021-10-06 12:33:57 -0300

    Final version of basic OLED lib thermometer

--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
 OLED.init(128, 64)
 let index = 0
-OLED.writeStringNewLine("  TEMPERATURA  ")
+OLED.writeStringNewLine("      TEMPERATURA  ")
 OLED.newLine()
 let tempNow = input.temperature()
 let min = tempNow
 let max = tempNow
-OLED.writeString(" TEMP: ")
+OLED.writeString(" LAST: ")
 OLED.writeNum(tempNow)
-OLED.writeStringNewLine("°C")
+OLED.writeStringNewLine("'C")
 OLED.newLine()
-OLED.drawLoading(index)
+OLED.writeString(" MIN:  ")
+OLED.writeNum(min)
+OLED.writeStringNewLine("'C")
+OLED.newLine()
+OLED.writeString(" MAX:  ")
+OLED.writeNum(max)
+OLED.writeStringNewLine("'C")
 basic.forever(function () {
     tempNow = input.temperature()
     if (tempNow > max) {
@@ -17,38 +23,22 @@ basic.forever(function () {
     } else if (tempNow < min) {
         min = tempNow
     }
-    index += 10
+    index += 1
     if (index == 100) {
         index = 0
         OLED.clear()
-        OLED.writeStringNewLine("  TEMPERATURA  ")
+        OLED.writeStringNewLine("      TEMPERATURA  ")
         OLED.newLine()
-        OLED.writeString(" TEMP: ")
+        OLED.writeString(" LAST: ")
         OLED.writeNum(tempNow)
-        OLED.writeStringNewLine("°C")
+        OLED.writeStringNewLine("'C")
         OLED.newLine()
-        OLED.drawLoading(index)
-    } else {
-        OLED.drawLoading(index)
-    }
-    if (input.buttonIsPressed(Button.A)) {
-        OLED.clear()
-        OLED.writeStringNewLine("  TEMPERATURA  ")
-        OLED.newLine()
-        OLED.writeString("MIN: ")
+        OLED.writeString(" MIN:  ")
         OLED.writeNum(min)
-        OLED.writeStringNewLine("°C")
+        OLED.writeStringNewLine("'C")
         OLED.newLine()
-        OLED.drawLoading(index)
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        OLED.clear()
-        OLED.writeStringNewLine("  TEMPERATURA  ")
-        OLED.newLine()
-        OLED.writeString("MAX: ")
+        OLED.writeString(" MAX:  ")
         OLED.writeNum(max)
-        OLED.writeStringNewLine("°C")
-        OLED.newLine()
-        OLED.drawLoading(index)
+        OLED.writeStringNewLine("'C")
     }
 })
